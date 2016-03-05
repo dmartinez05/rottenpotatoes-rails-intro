faefc648097177e763dcbb199aedc7f6f26abd86
dmartinez05 <dmartinez05@tamu.edu>
2016-03-05 02:50:44 +0000

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,6 +29,12 @@ class MoviesController < ApplicationController
       redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
 
+    if params[:ratings] != session[:ratings] and @selected_ratings != {}
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+
     @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
 
   end
